834381273a400a50a1b2a37e8c0fb7de94f03516
tnguyen303 <tringuyen552911@gmail.com>
2019-01-03 19:21:04 -0500

    Updates

---
+++ precache-manifest.2450c0227888e9d3c512f389cf40a278.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f048fbb21bd948f33da",
+    "url": "/homework-15/static/js/runtime~main.6f048fbb.js"
+  },
+  {
+    "revision": "c24a727bee88c8dccf17",
+    "url": "/homework-15/static/js/main.c24a727b.chunk.js"
+  },
+  {
+    "revision": "e9cdab68b4d9f7ed1ba4",
+    "url": "/homework-15/static/js/1.e9cdab68.chunk.js"
+  },
+  {
+    "revision": "c24a727bee88c8dccf17",
+    "url": "/homework-15/static/css/main.d98e5b12.chunk.css"
+  },
+  {
+    "revision": "dc569e02d61dd4f33a9a1a3327ca3184",
+    "url": "/homework-15/index.html"
+  }
+];
